7763b5c98eed27615039cb4a756a398fd908d971
Jungi <werwer748@naver.com>
2021-07-25 19:49:25 +0900

    이제 넥스트가 감이 좀 온다

---
+++ server-data/pages/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const App = () => {
+  const [username,setUsername] = useState("");
+  return(
+    <div>
+      <label>
+        username
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <p>{username} 깃허브 검색하기</p>
+      <Link href={`/users/${username}`}>
+        <a>검색하기</a>
+      </Link>
+    </div>
+  )
+};
+
+export default App;
---
+++ server-data/pages/users/[name].jsx
@@ -0,0 +1,49 @@
+import fetch from "isomorphic-unfetch";
+import Profile from "../components/Profile";
+import css from "styled-jsx/css";
+import Repositories from "../components/Repositories";
+
+
+const style = css`
+    .user-contents-wrapper {
+        display: flex;
+        padding: 20px;
+    }
+`;
+
+const name=({user, repos})=>{
+    return(
+        <div className="user-contents-wrapper">
+            <Profile user={user}/>
+            <Repositories user={user} repos={repos} />
+            <style jsx>{style}</style>
+        </div>
+    )
+}
+
+export const getServerSideProps = async({query}) => {
+    const { name, page } = query;
+   try {
+       let user;
+       let repos;
+
+       const userRes = await fetch(`https://api.github.com/users/${name}`);
+       if(userRes.status === 200){
+           user = await userRes.json();
+       }
+       const repoRes = await fetch(
+           `https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`
+       );
+       if(repoRes.status === 200){
+           repos = await repoRes.json();
+       }
+       console.log(repos);
+       return {props: {user, repos}};
+   }
+   catch(e){
+       console.log(e)
+       return{props:{}};
+   }
+}
+
+export default name;
